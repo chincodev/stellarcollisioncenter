53792447cae2e3a848893dd21a5fb9d8b1550001
Anthony Medina <anthonymedina958@gmail.com>
2024-07-22 17:09:09 -0400

    Fix: blog folder struct

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -4,17 +4,17 @@ import Breadcrumb from "../../components/breadcrumb/Breadcrumb";
 import Layout from "../../components/Layout";
 
 function BlogStandardPage() {
-  return (
-    <Layout
-      global={props.global}
-      seo_title={props.page.seo_title}
-      seo_description={props.page.seo_description}
-      og_card_image={props.global.settings.og_card_image}
-      url={props.global.settings.url+"/services/"+props.page.slug.current}
-    >
-      <Breadcrumb pageName="Blog Standard" pageTitle="Blog Standard" />
-      <BlogGrid />
-    </Layout>
+  return (<></>
+    // <Layout
+    //   global={props.global}
+    //   seo_title={props.page.seo_title}
+    //   seo_description={props.page.seo_description}
+    //   og_card_image={props.global.settings.og_card_image}
+    //   url={props.global.settings.url+"/services/"+props.page.slug.current}
+    // >
+    //   <Breadcrumb pageName="Blog Standard" pageTitle="Blog Standard" />
+    //   <BlogGrid />
+    // </Layout>
   );
 }
 
